962336d01e947345e8cd696fb0c45a7cfce4aa6a
Jabiyeff <cebiyevanar@gmail.com>
2020-09-17 16:54:12 +0400

    santoni: crDroid bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,8 +14,8 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_santoni.mk
+    $(LOCAL_DIR)/lineage_santoni.mk
 
 COMMON_LUNCH_CHOICES := \
-	arrow_santoni-userdebug \
-        arrow_santoni-user
+	lineage_santoni-userdebug \
+    lineage_santoni-user
